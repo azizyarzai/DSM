23f83c54b9ac73a56a6d75eb8dc8272675a8dd12
aziz.yarzai <azizulrahman.yarzai@gmail.com>
2020-06-06 00:09:09 +0530

    dsm renamed to src

--- src/dsm/wsgi.py
+++ src/dsm/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from dsm import settings
+from . import settings
 
 from django.core.wsgi import get_wsgi_application
 
