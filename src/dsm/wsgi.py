2d7a09c676474a62be9222588f0857f3312cb7df
aziz.yarzai <azizulrahman.yarzai@gmail.com>
2020-06-06 00:28:14 +0530

    tryinggggggg.....

--- src/dsm/wsgi.py
+++ src/dsm/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from dsm import settings
+
 
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "dsm.settings")
 
 application = get_wsgi_application()
