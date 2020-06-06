f9ac98869714ba926ba42c4dfcfaeacc48b7be54
azizyarzai <azizulrahman.yarzai@gmail.com>
2020-06-07 05:09:08 +0530

    customization added

--- deploy/update.sh
+++ deploy/update.sh
@@ -5,6 +5,7 @@ set -e
 PROJECT_BASE_PATH='/usr/local/apps/dsm'
 
 git pull
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart dsm
